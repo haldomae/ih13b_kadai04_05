443c2dedda0039ee2303e57c2487e4549c272727
haldomae <hal.domae@gmail.com>
2024-06-05 16:52:45 +0900

    クイズを出題する機能ï½を追加

--- app/src/main/java/com/hal_domae/ih13b_kadai04_05/MainActivity.kt
+++ app/src/main/java/com/hal_domae/ih13b_kadai04_05/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.hal_domae.ih13b_kadai04_05
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,6 +10,15 @@ import com.hal_domae.ih13b_kadai04_05.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
+    // ?を付ける事でnull許容型
+    private var rightAnswer: String? = null // クイズの正解
+    private var quizCount = 1 // 何問目か
+    private val quizData = mutableListOf(
+        listOf("膃肭臍", "おっとせい"),
+        listOf("馴鹿", "となかい"),
+        listOf("水豚", "かぴばら")
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -20,6 +30,21 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        showNextQuiz()
+    }
+
+    // クイズを出題するメソッド
+    private fun showNextQuiz(){
+        // 問題番号を表示
+//        val countLabel : TextView = findViewById(R.id.countLabel)
+//        countLabel.text = "もんだい"
+        binding.countLabel.text = getString(R.string.count_label, quizCount)
+
+        // クイズの問題をセット
+        val quiz = quizData[0]
+        binding.questionLabel.text = quiz[0]
+        // この問題の答え
+        rightAnswer = quiz[1]
 
     }
 }
